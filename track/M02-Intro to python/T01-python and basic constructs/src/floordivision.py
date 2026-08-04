78356675f237bb76b1f0b6acf23de50bda7a0520
RoshalDsouza24 <roshalds789@gmail.com>
2026-08-04 16:38:38 +0530

    floordivision

---
+++ track/M02-Intro to python/T01-python and basic constructs/src/floordivision.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+# Perform floor division
+result1 = first_number / second_number
+result2 = first_number // second_number
+
+# Display the result
+print(result1)
+print(result2)
