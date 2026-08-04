8dff0add3782a1af0bf05089b6fb6847bb6f0591
RoshalDsouza24 <roshalds789@gmail.com>
2026-08-04 15:28:09 +0530

    age calculation

---
+++ track/M02-Intro to python/T01-python and basic constructs/src/student_age_calculation.py
@@ -0,0 +1,10 @@
+current_age = "21"
+
+# Convert the string to an integer
+current_age = int(current_age)
+
+# Calculate next year's age
+next_age = current_age + 1
+
+# Display the result
+print(f"Age next year: {next_age}")
