84b2e2b9ac35a73c010fbfcd61230fa5dfa09c54
RoshalDsouza24 <roshalds789@gmail.com>
2026-08-07 16:19:51 +0530

    tuple without modifying

---
+++ track/M02-Intro to python/T03-Lists.tuples,strings,slicing,mutability/src/update-tuple-withoutmodifying.py
@@ -0,0 +1,8 @@
+course_name = input()
+course_week = input()
+course_status = input()
+updated_week = input()
+
+course_details = (course_name,course_week,course_status)
+course_details = (course_details[0],course_details[2],updated_week)
+print(course_details)
